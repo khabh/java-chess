4ce937ce89fac0f7a6def93318f0fb7d4c76b597
JUHA <84626225+khabh@users.noreply.github.com>
2024-03-31 15:30:48 +0900

    refactor: RuntimeException을 상속하도록 수정

--- src/main/java/chess/dao/DatabaseException.java
+++ src/main/java/chess/dao/DatabaseException.java
@@ -2,7 +2,7 @@ package chess.dao;
 
 import java.sql.SQLException;
 
-public class DatabaseException extends IllegalStateException {
+public class DatabaseException extends RuntimeException {
     private static final String ERROR_PREFIX = "[DATABASE_ERROR] ";
 
     public DatabaseException(String message, SQLException e) {
